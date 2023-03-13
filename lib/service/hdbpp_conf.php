9b48e807a332687e5ba27dc78ef4a538a0e088ba
Michael Schneider <mschneid@mbi-berlin.de>
2023-03-13 23:08:40 +0100

    use only default hdbpp docker conf

--- lib/service/hdbpp_conf.php
+++ lib/service/hdbpp_conf.php
@@ -1,30 +1,16 @@
 <?php
-	if (isset($_REQUEST['conf'])) {
-		if ($_REQUEST['conf']=='config_1') {
-			define("HOST", "host");
-			define("USERNAME", "username");
-			define("PASSWORD", "password");
-			define("DB", "db");
-			define("DBTYPE", "hdbpp");
-			define("LOG_REQUEST", "../../log/hdbpp_1.log");
-		}
-	}
-	else {
-		  define("HOST", "hostname");
-		  define("USERNAME", "username");
-		  define("PASSWORD", "password");
-		  define("DB", "hdbpp");
-		  define("DBTYPE", "hdbpp");
-		  // define("LOG_REQUEST", "../../log/hdbpp.log");
-	}
+define("HOST", "hdbpp-db");
+define("USERNAME", "hdbppro");
+define("PASSWORD", "hdbppro");
+define("DB", "hdbpp");
+define("DBTYPE", "hdbpp");
 
-  // if you wont to hide a part of your domain edit the following array
-  $skipdomain = array('tango://srv-tango-srf.fcs.elettra.trieste.it:20000/'=>'');
-  define("CSVREPO", 'https://myserver/csvrepo.php');
+// if you wont to hide a part of your domain edit the following array
+$skipdomain = array('tango://srv-tango-srf.fcs.elettra.trieste.it:20000/'=>'');
 
-  define("ALARM_HOST", "host");
-  define("ALARM_USER", "username");
-  define("ALARM_PASSWORD", "password");
-  define("ALARM_DB", "db");
+// define("ALARM_HOST", "host");
+// define("ALARM_USER", "username");
+// define("ALARM_PASSWORD", "password");
+// define("ALARM_DB", "db");
 
 ?>
